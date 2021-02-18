bc4f340e07c5c98c8dcaf5059881e338bc4d19cd
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-18 14:43:28 +1100

    create function orm where is inside the selectAll function

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,12 @@
 const connection = require('./connection.js');
+
+const orm = () => {
+    const selectALL =() => {
+        const query = "SELECT name FROM "
+    };
+};
+    
+
+
+
+module.exports = orm;
