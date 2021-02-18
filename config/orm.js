9aca1a9e9af8ea2c5c745441f6b8afc7763e24bb
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-18 14:50:17 +1100

    complete the function selectAll

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,14 @@
+const e = require('express');
 const connection = require('./connection.js');
 
 const orm = () => {
     const selectALL =(table , callBack) => {
         const query = `SELECT * FROM ${table}`;
+        connection.query(query , (error,result) => {
+            if(error)
+                throw error;
+            callBack(result);
+        });
     };
 };
     
