ea572f92c4cd610e4c26cce4faf1ff6b2ccf3b3f
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-18 12:12:23 +1100

    add 3 inserts to seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Pull-Beef with a lot');
+INSERT INTO burgers (name) VALUES ('Chicken with a lot');
+INSERT INTO burgers (name) VALUES ('Cheeseburger with pickles');
