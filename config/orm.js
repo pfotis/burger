a739a4e57e517fbf50cf7dc363887c18d9d45f6b
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-18 15:02:07 +1100

    add one more variable input to insertOne()

--- config/orm.js
+++ config/orm.js
@@ -4,14 +4,17 @@ const connection = require('./connection.js');
 const orm = () => {
     const selectALL =(table , callBack) => {
         const query = `SELECT * FROM ${table}`;
-        connection.query(query , (error,result) => {
+        connection.query(query , (error, result) => {
             if(error)
                 throw error;
             callBack(result);
         });
     };
-    const insertOne = (table, insertBurger) => {
-        const query = `INSERT INTO ${table} (name_burger) VALUES (${insertBurger})`
+    const insertOne = (table, division, insertBurger, callBack) => {
+        const query = `INSERT INTO ${table} (${division}) VALUES (${insertBurger})`;
+        connection.query(query , (error, result) => {
+            //code
+        });
     };
 };
     
