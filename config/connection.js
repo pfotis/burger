13fd2152b861d9d30e064932d257a7503b3cd3a4
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-18 12:31:25 +1100

    create a connection with the database burger_db

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,9 @@
 const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'p@ssworD',
+    database: 'burger_db',
+  });
