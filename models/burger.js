2b75305ab8ff2a01fe287d5209cdc6b1a9e65ca7
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-21 12:55:02 +1100

    create a update() in the file burger.js

--- models/burger.js
+++ models/burger.js
@@ -7,6 +7,9 @@ const burger = {
     insert(insertValue, cb){
         orm.insertOne('burger', 'burger_name', insertValue, (res) => cb(res));
     },
+    update(updateValue,  conditionValue, cb){
+        orm.updateOne('burger', 'devoured', updateValue, 'burger_name', conditionValue, (res) => cb(res));
+    },
 };
 
 module.exports = burger;
