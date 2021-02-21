9232de84af05c3b050f441034d9d4a9fa0f6f476
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-21 13:16:36 +1100

    create function router.get and router.post into burgers_controller.js file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,1 +1,20 @@
+const express = require('express');
 const burger = require('../models/burger.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) =>{
+    burger.all((data) =>{
+        const hbarsObject = {
+            burgers: data,
+          };
+          console.log('hbarsObject', hbarsObject);
+          res.render('index', hbarsObject);
+    });
+});
+
+router.post('/api/burgers', (req, res) => {
+    burger.insert(req.params.name,(result) => {
+        res.json({ id: result.insertId });
+    });
+});
