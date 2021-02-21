b9372ff40ac77e439a64731a89ad5f1e2a4b3caf
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-21 13:24:00 +1100

    create router.put finction and complete the file burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,3 +18,19 @@ router.post('/api/burgers', (req, res) => {
         res.json({ id: result.insertId });
     });
 });
+
+router.put('/api/burgers/:id', (req, res) => {
+    console.log(`id = ${req.params.id}`);
+  
+    cat.update(req.body.devoured, req.params.id, (result) => {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+      }
+    );
+  });
+  
+  // Export routes for server.js to use.
+  module.exports = router;
