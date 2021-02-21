be2df7c1cde184814de5dacfba71d1eaf462c98a
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-21 12:48:58 +1100

    create functions to burger.js all() and insert()

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,12 @@
 const orm = require('../config.orm.js');
+
+const burger = {
+    all(cb){
+        orm.selectAll('burger', (res) =>cb(res));
+    },
+    insert(insertValue, cb){
+        orm.insertOne('burger', 'burger_name', insertValue, (res) => cb(res));
+    },
+};
+
+module.exports = burger;
