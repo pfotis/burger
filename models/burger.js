c573fe14f2774bf6d9bb08ccbd039008640d8bca
pfotis <69961508+pfotis@users.noreply.github.com>
2021-02-21 14:13:58 +1100

    change name to table at file burger.js

--- models/burger.js
+++ models/burger.js
@@ -2,13 +2,13 @@ const orm = require('../config/orm.js');
 
 const burger = {
     all(cb){
-        orm.selectAll('burger', (res) =>cb(res));
+        orm.selectAll('burgers', (res) =>cb(res));
     },
     insert(insertValue, cb){
-        orm.insertOne('burger', 'burger_name', insertValue, (res) => cb(res));
+        orm.insertOne('burgers', 'burger_name', insertValue, (res) => cb(res));
     },
     update(updateValue,  conditionValue, cb){
-        orm.updateOne('burger', 'devoured', updateValue, 'burger_name', conditionValue, (res) => cb(res));
+        orm.updateOne('burgers', 'devoured', updateValue, 'burger_name', conditionValue, (res) => cb(res));
     },
 };
 
